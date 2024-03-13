68d7e69dabaeb98af090807bf7996749386a758b
rky-ke <ronaldyegon15@gmail.com>
2024-03-13 17:17:35 +0300

    Iterating through objects

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,4 +1,4 @@
 export default function iterateThroughObject(reportWithIterator) {
-    const joinedObjects = reportWithIterator.join("|");
-    return joinedObjects;
+  const joinedObjects = reportWithIterator.join('|');
+  return joinedObjects;
 }
